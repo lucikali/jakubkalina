a62eb34800bfa068398654d90649759ed20d7d4f
Dario Goutto <dario.goutto@gmail.com>
2016-07-02 21:22:45 +0100

    done

--- js/main.js
+++ js/main.js
@@ -13,8 +13,49 @@ $(document).ready(function(){
     $home.backstretch(url);
   }
 
+  function showBooksSection(section) {
+    var $activeBooks = $('.books-container.active'),
+      $booksSection = $('.books-container[data-books-section='+ section +']');
+
+    if ($activeBooks.data('books-section') === section) {
+      return;
+    }
+
+    $activeBooks.removeClass('active');
+    $booksSection.addClass('active');
+      $('[data-show-book-section='+ section +']').addClass('active');
+  }
+
+  $('[data-show-book-section]').on('click', function(e) {
+    var $t = $(this),
+      section = $t.data('show-book-section');
+
+    e.preventDefault();
+
+    if ($t.hasClass('active')) {
+      return;
+    }
+
+    $('.active[data-show-book-section]').removeClass('active');
+    showBooksSection(section);
+  });
+
+  $('[data-smooth-scroll]').on('click', function(e) {
+    var $t = $(this),
+      id = $t.attr('href'),
+      $elem = $(id);
+
+    if (!$elem.length) {
+      return;
+    }
+
+    $('body, html').animate({ scrollTop: $elem.offset().top });
+    e.preventDefault();
+  });
+
   $(window).on('resize', updateHomeHeight);
 
   updateHomeHeight();
   initHomeBackground();
+  showBooksSection('adult');
 });
